06ae0712742e5b496ad33e56e1ca4b77d2445e93
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-19 23:29:52 +0530

    User Controller updated with forgot password logic

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login, logout, getProfile } from "../controllers/user.controller.js"
+import { register, login, logout, getProfile, forgotPassword, resetPassword } from "../controllers/user.controller.js"
 import { isLoggedIn } from "../middlewares/auth.middleware.js"
 import upload from "../middlewares/multer.middleware.js";
 const router = Router();
@@ -8,5 +8,7 @@ router.post("/register", upload.single("avatar"), register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/me", isLoggedIn, getProfile);
+router.post("/forgotpassword", forgotPassword);
+router.post("/reset/:resetToken", resetPassword);
 
 export default router;
