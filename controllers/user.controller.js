a446d855a2b19de66b9076f037f4821beba762f5
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-20 20:30:51 +0530

    Updated user.controller.js recovery message

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -148,7 +148,7 @@ const forgotPassword = async (req, res, next) => {
     
     const subject = "Reset Password";
 
-    const message = `You can reset your password by clicking <a href=${resetPasswordURL} target="_blank" Reset your password> \n if the above link does not work for some reason then copy paste this link in new tab ${resetPasswordURL} \n If you have not requested this, kindly ignore `
+    const message = `You can reset your password by clicking <a href=${resetPasswordURL} target="_blank"> Reset your password</a> \n if the above link does not work for some reason then copy paste this link in new tab ${resetPasswordURL} \n If you have not requested this, kindly ignore `
 
     try {
         await sendEmail(email, subject, message);
