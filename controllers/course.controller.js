540d654149520da1213b56f58b98b10a52807278
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-22 13:48:19 +0530

    course.controller logic added

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -43,47 +43,72 @@ const createCourse = async (req, res, next) => {
         return next(new AppError("All fields are required", 400));
     }
 
-    try {
-        const course = await Course.create({
-            title,
-            description,
-            category,
-            createdBy,
-            thumbnail: {
-                public_id: "Dummy",
-                secure_url: "Dummy"
-            },
+    const course = await Course.create({
+        title,
+        description,
+        category,
+        createdBy,
+        thumbnail: {
+            public_id: "Dummy",
+            secure_url: "Dummy"
+        },
 
-        });
+    });
 
-        if (!course) {
-            return next(new AppError('Course could not created, please try again', 500));
-        }
-
-        if (req.file) {
+    if (!course) {
+        return next(new AppError('Course could not created, please try again', 500));
+    }
+    console.log(req.file);
+    if (req.file) {
+        try {
             const result = await cloudinary.v2.uploader.upload(req.file.path, {
                 folder: 'lms'
             });
-
+            console.log(JSON.stringify(result));
             if (result) {
                 course.thumbnail.public_id = result.public_id;
                 course.thumbnail.secure_url = result.secure_url;
             }
 
             fs.rm(`uploads/${req.file.filename}`);
-            await course.save();
+        } catch (e) {
+            return next(new AppError(e.message, 400));
         }
-        res.status(200).json({
-            success: true,
-            message: "course created successfully"
-        });
-    } catch (e) {
-        return next(new AppError(e.message, 400));
+        await course.save();
     }
+    res.status(200).json({
+        success: true,
+        message: "course created successfully",
+        course
+    });
 }
 
 const updateCourse = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const course = await Course.findByIdAndUpdate(
+            id,
+            {
+                $set: req.body
+            },
+            {
+                runValidators: true
+            }
+        );
 
+        if(!course) {
+            return next(
+                new AppError("Course with given id does not exist", 500)
+            );
+        }
+        return res.status(200).json({
+            success:true,
+            message: "Course updated Successfully!",
+            course
+        })
+    } catch (e) {
+        return next(new AppError(e.message,500));
+    }
 }
 
 const removeCourse = async (req, res, next) => {
