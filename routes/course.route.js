c68e73daef866b351dfb7932ef7ff51292775d73
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-22 17:04:34 +0530

    Adding authorization role middleware before course creation

--- routes/course.route.js
+++ routes/course.route.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { getAllCourses, getLecturesByCourseId, createCourse, updateCourse, removeCourse } from "../controllers/course.controller.js"; import { isLoggedIn } from "../middlewares/auth.middleware.js"
+import { getAllCourses, getLecturesByCourseId, createCourse, updateCourse, removeCourse } from "../controllers/course.controller.js";
+import { isLoggedIn, authorizedRoles } from "../middlewares/auth.middleware.js"
 import upload from "../middlewares/multer.middleware.js";
 const courseRoutes = Router();
 
 courseRoutes.route("/")
     .get(getAllCourses)
-    .post(upload.single('thumbnail'), createCourse);
+    .post(isLoggedIn,authorizedRoles("ADMIN"),upload.single('thumbnail'), createCourse);
 
 courseRoutes.route("/:id")
     .get(isLoggedIn, getLecturesByCourseId)
-    .put(updateCourse)
-    .delete(removeCourse);;
+    .put(isLoggedIn, updateCourse
+    )
+    .delete(isLoggedIn, removeCourse);;
 
 export default courseRoutes;
