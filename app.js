4f2fed109db0cfbe4202ddaf13c1ad02b02ade0d
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-01-18 23:26:25 +0530

    removed the / route

--- app.js
+++ app.js
@@ -44,10 +44,6 @@ app.use(cookieParser());
 
 app.use(morgan("dev"));
 
-app.use("/", function (req, res) {
-    res.send("Hello User !")
-})
-
 app.use("/ping", function (req, res) {
     res.send("Pong")
 })
